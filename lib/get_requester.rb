0f8b5145e428b443f3ee5097449cbf6e6fadd881
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-13 18:58:02 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -10,7 +10,7 @@ class GetRequester
 
   def get_response_body
     uri = URI.parse(@url)
-    response = Net::HTTP.get_response(@url)
+    response = Net::HTTP.get_response(uri)
     response.body
   end
 
